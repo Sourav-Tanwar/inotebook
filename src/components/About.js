4e331969ea5e63bdd3eab84702437b7c3a08a707
Sourav-Tanwar <souravtnw@gmail.com>
2023-03-02 19:02:15 +0530

    57 and 58 Video

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
+import noteContext from '../context/notes/noteContext'
 
 export const About = () => {
+  const a = useContext(noteContext)
+  useEffect(() => {
+    return () => {
+      a.update();
+      
+    }
+    // eslint-disable-next-line
+  }, [])
+  
   return (
-    <div>This is About</div>
+    <div>This is About {a.state.name} and he is on {a.state.class}</div>
   )
 }
 export default About
