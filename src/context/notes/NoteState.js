fd6a467f9209ea4f92d1ab85130a245b5c156db5
Sourav-Tanwar <souravtnw@gmail.com>
2023-03-03 10:25:44 +0530

    61 Video

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -28,6 +28,33 @@ const NoteState =(props)=>{
           "tag": "personal",
           "date": "2023-02-25T15:18:57.307Z",
           "__v": 0
+        },
+        {
+          "_id": "63f9e0a3113568e5d7e134c0",
+          "user": "63f9da9d4e103a0e0aed294d",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2023-02-25T10:19:15.541Z",
+          "__v": 0
+        },
+        {
+          "_id": "63fa2531112a9bfff385119e",
+          "user": "63f9da9d4e103a0e0aed294d",
+          "title": "My Title1",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2023-02-25T15:11:45.820Z",
+          "__v": 0
+        },
+        {
+          "_id": "63fa26e1e53e7353364a6639",
+          "user": "63f9da9d4e103a0e0aed294d",
+          "title": "My Title1 updated",
+          "description": "Please wake up early updated",
+          "tag": "personal",
+          "date": "2023-02-25T15:18:57.307Z",
+          "__v": 0
         }
       ]
       const [notes, setnotes]= useState(notesInitial)
