a2804bb343efbec10ec4322189f4c4ab43d1b213
Sourav-Tanwar <souravtnw@gmail.com>
2023-03-04 12:58:58 +0530

    64 Video

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -74,11 +74,13 @@ const NoteState =(props)=>{
         setNotes(notes.concat(note))
       }
       //Delete a Note
-      const deleteNote = ()=>{
-        
+      const deleteNote = (id)=>{  
+        console.log("Deleting the note with id" + id)
+        const newNotes = notes.filter((note)=>{return note._id!==id})
+        setNotes(newNotes)
       }
       //Edit a Note
-      const editNote = ()=>{
+      const editNote = (id,title, description,tag)=>{
         
       }
     return(
