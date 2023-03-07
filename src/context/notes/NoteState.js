30608317d2ad7e56a75539eb20f239a9c4d409de
Sourav-Tanwar <souravtnw@gmail.com>
2023-03-07 10:08:28 +0530

    67 Video

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -59,26 +59,29 @@ const NoteState = (props) => {
   const editNote = async (id, title, description, tag) => {
     // API Call
     const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: "POST",
+      method: "PUT",
       headers: {
         "Content-Type": "application/json",
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjNmOWRhOWQ0ZTEwM2EwZTBhZWQyOTRkIn0sImlhdCI6MTY3NzMzODM3NH0.STJvcF9rcHnKTeuwjiV0NU0bkgP9maCDvQUfgPbkTgg"
       },
       body: JSON.stringify({title,description,tag}),
     });
-    const json = response.json();
+    const json = await response.json();
     console.log(json)
 
+    let newNotes = JSON.parse(JSON.stringify(notes))
     //Logic to edit client
-    for (let index = 0; index < notes.length; index++) {
-      const element = notes[index];
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
       }
-
+      
     }
+    setNotes(newNotes)
   }
   return (
     <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote,getNotes }}>
